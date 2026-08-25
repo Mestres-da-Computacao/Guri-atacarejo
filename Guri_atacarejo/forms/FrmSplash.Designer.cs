be1cf4ab0ab73a0c87ee34114c59e51c53257380
PaulinPerera <paulinperera007@gmail.com>
2026-08-25 12:19:58 -0300

    feat: botano o a imagem no splash scream

--- Guri_atacarejo/forms/FrmSplash.Designer.cs
+++ Guri_atacarejo/forms/FrmSplash.Designer.cs
@@ -28,13 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(119)))), ((int)(((byte)(27)))));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-38, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(705, 394);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // SplashScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(667, 393);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -42,11 +58,14 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.FrmSplash);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Guri_atacarejo/forms/FrmSplash.cs
+++ Guri_atacarejo/forms/FrmSplash.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Media;
 using System.Text;
@@ -17,6 +18,18 @@ namespace Guri_atacarejo
         public SplashScreen()
         {
             InitializeComponent();
+            int raio = 10;
+
+            GraphicsPath path = new GraphicsPath();
+
+            path.AddArc(0, 0, raio, raio, 180, 90);
+            path.AddArc(Width - raio, 0, raio, raio, 270, 90);
+            path.AddArc(Width - raio, Height - raio, raio, raio, 0, 90);
+            path.AddArc(0, Height - raio, raio, raio, 90, 90);
+
+            path.CloseFigure();
+
+            this.Region = new Region(path);
         }
 
         private Timer timer;
