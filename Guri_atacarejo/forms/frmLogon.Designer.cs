ac0a62f83e882478999bf81dc3c072dcc485d741
Fvin-117 <flavioviniciuslacerda@gmail.com>
2026-09-08 12:09:08 -0300

    mudei um pouco a interface

--- Guri_atacarejo/forms/frmLogon.Designer.cs
+++ Guri_atacarejo/forms/frmLogon.Designer.cs
@@ -34,6 +34,7 @@
             this.Usertxtbox = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -75,7 +76,7 @@
             // 
             this.button1.BackColor = System.Drawing.Color.Transparent;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(270, 397);
+            this.button1.Location = new System.Drawing.Point(342, 397);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(137, 47);
             this.button1.TabIndex = 3;
@@ -88,7 +89,7 @@
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(119)))), ((int)(((byte)(27)))));
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.button2.Location = new System.Drawing.Point(256, 344);
+            this.button2.Location = new System.Drawing.Point(256, 356);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(165, 25);
             this.button2.TabIndex = 4;
@@ -96,12 +97,24 @@
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.BtnRecupSenha_Click);
             // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(188, 397);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 47);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Cancelar";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
             // FrmLogon
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(119)))), ((int)(((byte)(27)))));
             this.ClientSize = new System.Drawing.Size(667, 456);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.Usertxtbox);
@@ -122,5 +135,6 @@
         private System.Windows.Forms.TextBox Usertxtbox;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
