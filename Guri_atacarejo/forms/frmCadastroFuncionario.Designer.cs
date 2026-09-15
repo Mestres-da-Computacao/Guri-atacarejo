afd59d6172641d128740451684f9c7a647e35421
Fvin-117 <flavioviniciuslacerda@gmail.com>
2026-09-15 20:14:26 -0300

    mudei o char da senha

--- Guri_atacarejo/forms/frmCadastroFuncionario.Designer.cs
+++ Guri_atacarejo/forms/frmCadastroFuncionario.Designer.cs
@@ -193,6 +193,7 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(71, 135);
             this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(273, 26);
             this.textBox2.TabIndex = 5;
             // 
